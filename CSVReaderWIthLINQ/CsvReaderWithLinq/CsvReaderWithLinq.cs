64da36b193b117da945ea0940af12d3b6a03f595
MjDDoesThings <134036061+MjDDoesThings@users.noreply.github.com>
2023-05-19 09:15:44 +0400

    Uploading files to the Master Branch
    This is the source code

---
+++ CSVReaderWIthLINQ/CsvReaderWithLinq/CsvReaderWithLinq.cs
@@ -0,0 +1,44 @@
+﻿namespace CsvReaderWithLinq
+{
+    public static class CsvReaderWithLinq
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("How to Operate: Either Use Visual Studio Compiler or a command line Arguement by entering the following arguements:" +
+                    " CsvReaderWithLinq.exe <csvFilePath> <columnNumber> <searchKey>");
+
+                return;
+            }
+
+            var csvFilePath = args[0];
+            var columnNumber = int.Parse(args[1]);
+            var searchKey = args[2];
+
+            try
+            {
+                var lines = File.ReadAllLines(csvFilePath);
+                var matchingLine = lines.FirstOrDefault(line =>
+                {
+                    var values = line.Split(',');
+
+                    return values.Length > columnNumber && values[columnNumber] == searchKey;
+                });
+
+                if (matchingLine == null)
+                {
+                    Console.WriteLine("No matching entry found.");
+                }
+                else
+                {
+                    Console.WriteLine(matchingLine);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
